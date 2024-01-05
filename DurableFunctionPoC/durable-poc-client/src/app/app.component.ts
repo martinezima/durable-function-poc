a68f0c43d72c10cd20faa6714d7d1824056a36b6
Miguel Martinez <martinezi.miguel@gmail.com>
2024-01-05 10:45:47 -0600

    Cancellation runbook client implemetation done.

--- DurableFunctionPoC/durable-poc-client/src/app/app.component.ts
+++ DurableFunctionPoC/durable-poc-client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DurableFuctionService } from './durable-fuction.service';
 import { BehaviorSubject, Subject, startWith } from 'rxjs';
 import { ICustomStatus } from './models/custom-status';
+import { IDurableFunctionClientRequest } from './models/durable-function-client-request';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,20 @@ import { ICustomStatus } from './models/custom-status';
 })
 export class AppComponent {
   title = 'Durable PoC Client';
-  instanceId = null;
+  instanceId: string = '';
   constructor(public durableFuctionService: DurableFuctionService) {
 
   }
 
   public startDurableFunction(): void {
     this.durableFuctionService.durableFunction$.subscribe(durableFuctionResponse => this.startMonitoring(durableFuctionResponse));
+  }
+
+  public cancelDurableFunction(): void {
+    const request: IDurableFunctionClientRequest = {
+      instanceId: this.instanceId
+    };
+    this.durableFuctionService.cancelRunbookProcess(request).subscribe();
   }
   
   public startMonitoring(durableFuctionResponse: any): void { 
--- DurableFunctionPoC/durable-poc-client/src/app/durable-fuction.service.ts
+++ DurableFunctionPoC/durable-poc-client/src/app/durable-fuction.service.ts
@@ -3,17 +3,20 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { BehaviorSubject, catchError, combineLatest, map, merge, Observable,
    Subject, tap, scan, throwError, shareReplay, delay, retryWhen, expand, EMPTY } from 'rxjs';
 import { ICustomStatus } from './models/custom-status';
+import { IDurableFunctionClientRequest } from './models/durable-function-client-request';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DurableFuctionService {
   private durableFunctionPocUrl = 'api/ProccessRunbookStarter';
+  private cancellationDurableFunctionPocUrl = 'api/CancelRunbook';
   private monitoringPocUrl = 'runtime/';
   private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   private monitoringRunbookInstanceBs = new BehaviorSubject<ICustomStatus[]>([]);
   public monitoringRunbookInstance$ = this.monitoringRunbookInstanceBs.asObservable();
   public monitoringComplete: boolean = false;
+  public monitoringCancell: boolean = false;
   
   constructor(private http:HttpClient) { }
 
@@ -25,40 +28,41 @@ export class DurableFuctionService {
   }, { headers: this.headers})
   .pipe(tap(result => console.log('result: ', result)));
 
-
-  public retryUrlUntilMatchCondition(monitoringUri: string): Observable<any> {
-    // const val = this.monitoringRunbookInstanceBs.getValue();
-    // val.push( {
-    //   Status: "NotStarted",
-    //   Activity: "Not Started yet.",
-    //   Details: []
-    // });
-    // this.monitoringRunbookInstanceBs.next(val);     
+  public retryUrlUntilMatchCondition(monitoringUri: string): Observable<any> {  
     const monitoring$ = this.monitoringDurableFunction(monitoringUri);
     return monitoring$
     .pipe(
       map(response => {
         console.log(response);
         if (response.customStatus) {
-          this.monitoringRunbookInstanceBs.next(response.customStatus);
+          const customStatus = Array.isArray(response.customStatus) ? response.customStatus : [response.customStatus];
+          this.monitoringRunbookInstanceBs.next(customStatus);
         }
         return response;
       }),      
       expand(response => 
-        response && response.runtimeStatus !== "Completed" ? 
+        response && (response.runtimeStatus !== "Completed" &&
+        response.runtimeStatus !== "Terminated") ? 
         this.monitoringDurableFunction(monitoringUri)
         .pipe(
           delay(2000),
           map(response => {
             console.log(response);
             if (response.customStatus) {
-              this.monitoringRunbookInstanceBs.next(response.customStatus);
+              const customStatus = Array.isArray(response.customStatus) ? response.customStatus : [response.customStatus];
+              this.monitoringRunbookInstanceBs.next(customStatus);
             }
             return response;
           })
         )        
         : this.completeMonitoring())
     );
+  }
+
+  public cancelRunbookProcess(dfClientRequest: IDurableFunctionClientRequest): Observable<any> {
+    return this.http.post<any>(this.cancellationDurableFunctionPocUrl,dfClientRequest,
+      { headers: this.headers})
+      .pipe(map(() => this.monitoringCancell = true));
   }
   
   private monitoringDurableFunction(monitoringUri: string): Observable<any> {   
